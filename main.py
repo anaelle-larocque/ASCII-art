3b1655f7995ba5188483447a5e9b2a5500c4be48
Anaelle-Larocque <anaelle.larocque@edu.esiee.fr>
2025-10-03 08:15:19 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,51 +1,50 @@
 #### Imports et définition des variables globales
 
 # Mandatory for the recursive solution to work on large inputs
+"""
+Module principal pour la génération d'art ASCII et l'encodage de chaînes.
+"""
+#### Imports et définition des variables globales
+
+# Mandatory for the recursive solution to work on large inputs
 import sys
 sys.setrecursionlimit(2000)
 
-
 #### Fonctions secondaires
 
-
-def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
-
-    Args:
-        s (str): la chaîne de caractères à encoder
-
-    Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
+def artcode_i(s: str):
     """
-    
-    # votre code ici
-
-    return [ ]
-
-
-def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
-
-    Args:
-        s (str): la chaîne de caractères à encoder
-
-    Returns:
-        list: la liste des tuples (caractère, nombre d'occurences)
+    Fonction d'encodage itératif pour l'art ASCII.
     """
-    
-    # votre code ici
+    if s == "":
+        return []
+    chars = [s[0]]
+    occurences = [1]
+    for k in range(1, len(s)):
+        if s[k] == s[k-1]:
+            occurences[-1] += 1
+        else:
+            chars.append(s[k])
+            occurences.append(1)
+    return list(zip(chars, occurences))
 
-    # cas de base
-    # recherche nombre de caractères identiques au premier
-    # appel récursif
-
-    return []
-    
+def artcode_r(s: str):
+    """ 
+    Fonction d'encodage récursif pour l'art ASCII.
+    """
+    if s == "":
+        return []
+    premier = s[0]
+    tuple_premier = (premier, ord(premier))
+    reste_code = artcode_r(s[1:])
+    return [tuple_premier] + reste_code
 
 #### Fonction principale
 
-
 def main():
+    """
+    Fonction principale pour tester les encodages ASCII-art.
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
